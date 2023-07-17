ade13d2e0544394d3976661ff4334d32033a2477
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-07-17 18:32:33 -0300

    feat: add NUTANIX kernel person

--- kernel-script/010-build-kernel.sh
+++ kernel-script/010-build-kernel.sh
@@ -20,53 +20,55 @@ if [ -n "$(ls -A /usr/src)" ]; then
     rm -rf /usr/src
 fi
 sync
-sleep 100
+sleep 60
 git clone -b releng/13.2 --depth 1 https://git.freebsd.org/src.git /usr/src 
 
+#
+# Define make options
+#
+cat <<EOF>> /etc/make.conf
+# run the build and installation commands in a non-interactive way
+ BATCH=yes
 
-touch /etc/make.conf
-{
-  echo
-  echo \# run the build and installation commands in a non-interactive way
-  echo BATCH=yes
-  echo
-  echo \# native CPU type
-  echo CPUTYPE?=native
-  echo \# CPU optimations
-  echo CFLAGS=-O2 -pipe -fno-strict-aliasing
-  echo COPTFLAGS=-O2 -pipe -fno-strict-aliasing
-  echo \# enable speed
-  echo MALLOC_PRODUCTION=\"YES\"
-} > /etc/make.conf
+# native CPU type
+ CPUTYPE?=native
 
-touch /etc/src.conf
-{
-    echo \# NO DEBUG
-    echo WITHOUT_ASSERT_DEBUG=\"ON\"
-} > /etc/src.conf
+# CPU optimations
+ CFLAGS=-O2 -pipe -fno-strict-aliasing
+ COPTFLAGS=-O2 -pipe -fno-strict-aliasing
+ 
+# enable speed
+ MALLOC_PRODUCTION="YES"
+EOF
 
-touch /usr/src/sys/amd64/conf/CUSTOM-KERNEL
-{
-  echo
-  echo include GENERIC
-  echo
-  echo ident CUSTOM-KERNEL
-  echo
-  echo \# Add enable IPFW and NAT
-  echo options  IPFIREWALL                    \# enables IPFW
-  echo options  IPFIREWALL_VERBOSE            \# enables logging for rules with log keyword
-  echo options  IPFIREWALL_VERBOSE_LIMIT=256  \# limits number of logged packets per-entry
-  echo
-  echo options  IPDIVERT                      \# enables NATd Support
-  echo options  IPFIREWALL_NAT                \# IPFW in-Kernel NAT support
-  echo options  LIBALIAS                      \# required for in-Kernel NAT / replacement for NATd
-  echo
-  echo \# Activate the TCP BBR
-  echo makeoptions WITH_EXTRA_TCP_STACKS=1    \# ADD EXTRA TCP STACKS
-  echo options RATELIMIT
-  echo options TCPHPTS
+cat <<EOF>> /etc/src.conf
+# NO DEBUG
+  WITHOUT_ASSERT_DEBUG="ON"
+EOF
 
-} > /usr/src/sys/amd64/conf/CUSTOM-KERNEL
+cp ./NUTANIX_KERNEL  /usr/src/sys/amd64/conf
+
+cat <<EOF>> /usr/src/sys/amd64/conf/CUSTOM-KERNEL
+# Extend Nutanix conf
+include NUTANIX
+
+ident CUSTOM-KERNEL
+
+# Add enable IPFW and NAT
+options  IPFIREWALL                    # enables IPFW
+options  IPFIREWALL_VERBOSE            # enables logging for rules with log keyword
+options  IPFIREWALL_VERBOSE_LIMIT=256  # limits number of logged packets per-entry
+
+options  IPDIVERT                      # enables NATd Support
+options  IPFIREWALL_NAT                # IPFW in-Kernel NAT support
+options  LIBALIAS                      # required for in-Kernel NAT / replacement for NATd
+
+# Activate the TCP BBR
+makeoptions WITH_EXTRA_TCP_STACKS=1    # ADD EXTRA TCP STACKS
+options RATELIMIT
+options TCPHPTS
+
+EOF
 
 cd /usr/src
 
