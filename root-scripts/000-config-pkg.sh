7907e233c8e99d32750f61e5fa36891a2f3c7da2
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-07-19 10:12:06 -0300

    fix: snapshot create

--- root-scripts/000-config-pkg.sh
+++ root-scripts/000-config-pkg.sh
@@ -7,6 +7,22 @@ echo
 echo Make Snapshot
 echo
 
+
+SNAPSHOT_NAME="default@BeforeConfig"
+
+# Verifica se o snapshot já existe
+if zfs list -t snapshot | grep -q "$SNAPSHOT_NAME"; then
+    echo "O snapshot $SNAPSHOT_NAME já existe."
+else
+    zfs snapshot -r "$answer"/ROOT/"$SNAPSHOT_NAME"
+    if [ $? -eq 0 ]; then
+        echo "Snapshot $SNAPSHOT_NAME criado com sucesso."
+    else
+        echo "Erro ao criar o snapshot $SNAPSHOT_NAME."
+    fi
+fi
+
+
 echo "Enter you ZPOOL name: "
 read -r answer
 zfs snapshot -r "$answer"/ROOT/default@BeforeConfig
